cabf252e4f2e8674a3f9e5e46db3a534189c2071
raphahenrique <rhf4pf@mail.umkc.edu>
2016-12-22 18:58:32 -0200

    Sending and Receiving commands

--- projeto1/daemon.py
+++ projeto1/daemon.py
@@ -9,13 +9,14 @@ else:
 
     porta = int(sys.argv[2])
     ip = '127.0.0.1'
-    buffer_size = 1024
+    buffer_size = 10024
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((ip,porta))
+    s.timeout(15.0)
 
     while True:
-        s.listen(1)
+        s.listen(3)
         conn, addr = s.accept()
         print 'Connection address: ', addr
 
@@ -25,27 +26,44 @@ else:
 
             header = Header(serialialized_header)
             if header.protocol == 1:
+                args = header.options.replace('|', '').replace(';', '').replace('>', '')
+
+                print 'args', args
+                out = subprocess.check_output(["ps", args])
+
+                print 'out',out
+                s.sendall(out)
+
+            if header.protocol == 2:
+                args = header.options.replace('|', '').replace(';', '').replace('>', '')
+
+                print 'args', args
+                out = subprocess.check_output(["df", args])
+
+                print 'out',out
+                s.sendall(out)
+
+            if header.protocol == 3:
+                args = header.options.replace('|', '').replace(';', '').replace('>', '')
+
+                print 'args', args
+                out = subprocess.check_output(["df", args])
+
+                print 'out',out
+                s.sendall(out)
+
+            if header.protocol == 4:
+                args = header.options.replace('|', '').replace(';', '').replace('>', '')
+
+                print 'args', args
+                out = subprocess.check_output(["uptime", args])
+
+                print 'out',out
+                s.sendall(out)
 
 
+            print 'Received data: ', serialialized_header
 
 
-            if data == "TESTE":
-                '''
-                import subprocess
-                p = subprocess.Popen("date", stdout=subprocess.PIPE, shell=True)
-                (output, err) = p.communicate()
-                print "Today is", output
-
-                ou
-
-                out = check_output(["ntpq", "-p"])
-                '''
-
-                out = subprocess.check_output('ps')
-
-                print 'Received data: ', data
-                print 'Command ps executed: ', out
-
-
-        conn.close()
+        #conn.close()
         break
--- projeto1/webserver.py
+++ projeto1/webserver.py
@@ -53,6 +53,7 @@ print '</head>'
 print '<body style="background-image:url(http://www.evohosting.co.uk/wp-content/uploads/2014/12/Pattern-christmas-elements-seamless-vector-011-e1418392819694.jpg); background-repeat: repeat;">'
 print '<div style="background-color: #ffffff; height:100%; width:100%; opacity: 0.9; z-index:-1; position: fixed;"> </div>'
 
+#MAQUINA 1
 header_maq1_ps = Header()
 header_maq1_df = Header()
 header_maq1_finger = Header()
@@ -67,18 +68,53 @@ header_maq1_finger.options = maq1.finger_args
 header_maq1_uptime.protocol = 4
 header_maq1_uptime.options = maq1.uptime_args
 
+#MAQUINA 2
+header_maq2_ps = Header()
+header_maq2_df = Header()
+header_maq2_finger = Header()
+header_maq2_uptime = Header()
+
+header_maq2_ps.protocol = 1
+header_maq2_ps.options = maq2.ps_args
+header_maq2_df.protocol = 2
+header_maq2_df.options = maq2.df_args
+header_maq2_finger.protocol = 3
+header_maq2_finger.options = maq2.finger_args
+header_maq2_uptime.protocol = 4
+header_maq2_uptime.options = maq2.uptime_args
+
+
+#MAQUINA 3
+header_maq3_ps = Header()
+header_maq3_df = Header()
+header_maq3_finger = Header()
+header_maq3_uptime = Header()
+
+header_maq3_ps.protocol = 1
+header_maq3_ps.options = maq3.ps_args
+header_maq3_df.protocol = 2
+header_maq3_df.options = maq3.df_args
+header_maq3_finger.protocol = 3
+header_maq3_finger.options = maq3.finger_args
+header_maq3_uptime.protocol = 4
+header_maq3_uptime.options = maq3.uptime_args
+
+
 ip = '127.0.0.1'
 pm1 = 9001
 pm2 = 9002
 pm3 = 9003
-buff_size = 1024
+buff_size = 10024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((ip,pm1))
+try:
+    s.connect((ip,pm1))
+except socket.error:
+    print('connection error')
 
 if maq1.ps != '':
     s.send(header_maq1_ps.make_header())
-    maq1.ps_result = s.recv(buff_size)
+    maq1.ps_result = s.recv(1024)
 if maq1.df != '':
     s.send(header_maq1_df.make_header())
     maq1.df_result = s.recv(buff_size)
@@ -88,6 +124,72 @@ if maq1.finger != '':
 if maq1.uptime != '':
     s.send(header_maq1_uptime.make_header())
     maq1.uptime_result = s.recv(buff_size)
+
+
+#Maquina2
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.connect((ip, pm2))
+except socket.error:
+    print('connection error')
+
+if maq2.ps != '':
+    s.send(header_maq2_ps.make_header())
+    maq2.ps_result = s.recv(buff_size)
+if maq2.df != '':
+    s.send(header_maq2_df.make_header())
+    maq2.df_result = s.recv(buff_size)
+if maq2.finger != '':
+    s.send(header_maq2_finger.make_header())
+    maq2.finger_result = s.recv(buff_size)
+if maq2.uptime != '':
+    s.send(header_maq2_uptime.make_header())
+    maq2.uptime_result = s.recv(buff_size)
+
+
+#Maquina3
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.connect((ip, pm3))
+except socket.error:
+    print('connection error')
+
+if maq3.ps != '':
+    s.send(header_maq3_ps.make_header())
+    maq3.ps_result = s.recv(buff_size)
+if maq3.df != '':
+    s.send(header_maq3_df.make_header())
+    maq3.df_result = s.recv(buff_size)
+if maq3.finger != '':
+    s.send(header_maq3_finger.make_header())
+    maq3.finger_result = s.recv(buff_size)
+if maq3.uptime != '':
+    s.send(header_maq3_uptime.make_header())
+    maq3.uptime_result = s.recv(buff_size)
+
+
+'''
+try:
+
+    # Send data
+    s.sendall(header_maq1_ps.make_header())
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(header_maq1_ps.make_header())
+
+    while amount_received < amount_expected:
+        data = s.recv(buff_size)
+        amount_received += len(data)
+
+    maq1.ps_result = s.recv(buff_size)
+
+finally:
+    print >> sys.stderr, 'closing socket'
+    sock.close()
+
+'''
+
 s.close()
 
 print maq1
