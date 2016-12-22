4de5c8e96fc03959ea700c54b639255ce28c7a38
Gabriel Palomino <palominogabriel@hotmail.com>
2016-12-22 13:33:01 -0200

    Header working with variable size in options field

--- projeto1/header.py
+++ projeto1/header.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import binascii
+
 
 class Header():
     def __init__(self, header=None):
@@ -14,8 +16,7 @@ class Header():
         self.__checksum_len = 16  # bits
         self.__source_addr_len = 32  # bits
         self.__destination_addr_len = 32  # bits
-        self.__options_len = 24  # bits
-        self.__paddin_len = 8  # bits
+        self.__options_len = 32  # bits
 
         if header == None:
             self.__version = 2
@@ -30,213 +31,226 @@ class Header():
             self.__checksum = 0
             self.__source_addr = '127.0.0.1'
             self.__destination_addr = '127.0.0.1'
-            self.__options = 0
-            self.__paddin = 0
+            self.__options = ''
         else:
             self.break_header(header)
 
     @property
-    def h_version(self):
+    def version(self):
         return self.__version
 
-    @h_version.setter
-    def h_version(self, value):
+    @version.setter
+    def version(self, value):
         self.__version = value
 
     @property
-    def h_IHL(self):
+    def IHL(self):
         return self.__IHL
 
-    @h_IHL.setter
-    def h_IHL(self, value):
+    @IHL.setter
+    def IHL(self, value):
         self.__IHL = value
 
     @property
-    def h_type_service(self):
+    def type_service(self):
         return self.__type_service
 
-    @h_type_service.setter
-    def h_type_service(self, value):
+    @type_service.setter
+    def type_service(self, value):
         self.__type_service = value
 
     @property
-    def h_total_length(self):
+    def total_length(self):
         return self.__total_length
 
-    @h_total_length.setter
-    def h_total_length(self, value):
+    @total_length.setter
+    def total_length(self, value):
         self.__total_length = value
 
     @property
-    def h_identification(self):
+    def identification(self):
         return self.__identification
 
-    @h_identification.setter
-    def h_identification(self, value):
+    @identification.setter
+    def identification(self, value):
         self.__identification = value
 
     @property
-    def h_flags(self):
+    def flags(self):
         return self.__flags
 
-    @h_flags.setter
-    def h_flags(self, value):
+    @flags.setter
+    def flags(self, value):
         self.__flags = value
 
     @property
-    def h_fragment_offset(self):
+    def fragment_offset(self):
         return self.__fragment_offset
 
-    @h_fragment_offset.setter
-    def h_fragment_offset(self, value):
+    @fragment_offset.setter
+    def fragment_offset(self, value):
         self.__fragment_offset = value
 
     @property
-    def h_time_live(self):
+    def time_live(self):
         return self.__time_live
 
-    @h_time_live.setter
-    def h_time_live(self, value):
+    @time_live.setter
+    def time_live(self, value):
         self.__time_live = value
 
     @property
-    def h_protocol(self):
+    def protocol(self):
         return self.__protocol
 
-    @h_protocol.setter
-    def h_protocol(self, value):
+    @protocol.setter
+    def protocol(self, value):
         self.__protocol = value
 
     @property
-    def h_checksum(self):
+    def checksum(self):
         return self.__checksum
 
-    @h_checksum.setter
-    def h_checksum(self, value):
+    @checksum.setter
+    def checksum(self, value):
         self.__checksum = value
 
     @property
-    def h_source_addr(self):
+    def source_addr(self):
         return self.__source_addr
 
-    @h_source_addr.setter
-    def h_source_addr(self, value):
+    @source_addr.setter
+    def source_addr(self, value):
         self.__source_addr = value
 
     @property
-    def h_destination_addr(self):
+    def destination_addr(self):
         return self.__destination_addr
 
-    @h_destination_addr.setter
-    def h_destination_addr(self, value):
+    @destination_addr.setter
+    def destination_addr(self, value):
         self.__destination_addr = value
 
     @property
-    def h_options(self):
+    def options(self):
         return self.__options
 
-    @h_options.setter
-    def h_options(self, value):
+    @options.setter
+    def options(self, value):
         self.__options = value
 
-    @property
-    def h_padding(self):
-        return self.__padding
-
-    @h_padding.setter
-    def h_padding(self, value):
-        self.__padding = value
+    #Fields Length
 
     @property
-    def h_version_len(self):
+    def version_len(self):
         return self.__version_len
 
     @property
-    def h_IHL_len(self):
+    def IHL_len(self):
         return self.__IHL_len
 
     @property
-    def h_type_service_len(self):
+    def type_service_len(self):
         return self.__type_service_len
 
     @property
-    def h_total_length_len(self):
+    def total_length_len(self):
         return self.__total_length_len
 
     @property
-    def h_identification_len(self):
+    def identification_len(self):
         return self.__identification_len
 
     @property
-    def h_flags_len(self):
+    def flags_len(self):
         return self.__flags_len
 
     @property
-    def h_fragment_offset_len(self):
+    def fragment_offset_len(self):
         return self.__fragment_offset_len
 
     @property
-    def h_time_live_len(self):
+    def time_live_len(self):
         return self.__time_live_len
 
     @property
-    def h_protocol_len(self):
+    def protocol_len(self):
         return self.__protocol_len
 
     @property
-    def h_checksum_len(self):
+    def checksum_len(self):
         return self.__checksum_len
 
     @property
-    def h_source_addr_len(self):
+    def source_addr_len(self):
         return self.__source_addr_len
 
     @property
-    def h_destination_addr_len(self):
+    def destination_addr_len(self):
         return self.__destination_addr_len
 
     @property
-    def h_options_len(self):
+    def options_len(self):
         return self.__options_len
 
-    def addr_to_bin(self, addr=''):
-        splited_addr = addr.split('.')
-        binary = ''
-        for spt in splited_addr:
-            binary += str(format(int(spt), '#010b').replace('0b',''))
-        return binary
+    @options_len.setter
+    def options_len(self,value):
+        self.__options_len = value
 
+    def addr_to_bin(self, addr=''):
+        if addr != '':
+            splited_addr = addr.split('.')
+            binary = ''
+            for spt in splited_addr:
+                binary += str(format(int(spt), '#010b').replace('0b',''))
+            return binary
+        else:
+            return addr
+
+    def bin_to_addr(self,addr=''):
+        if addr != '':
+            n1 = int(addr[:8],2)
+            n2 = int(addr[8:16],2)
+            n3 = int(addr[16:24],2)
+            n4 = int(addr[24:32],2)
+            return str(n1) + '.' + str(n2) + '.' + str(n3) + '.' + str(n4)
+        else:
+            return addr
     def make_header(self):
         header = ''
-        header += str(format(self.__version,'#0' + str(self.__version_len + 2) + 'b').replace('0b',''))
-        header  += str(format(self.__IHL,'#0' + str(self.__IHL_len + 2) + 'b').replace('0b',''))
-        header  += str(format(self.__type_service, '#0' + str(self.__type_service_len + 2) + 'b').replace('0b', ''))
-        header  += str(format(self.__total_length, '#0' + str(self.__total_length_len + 2) + 'b').replace('0b', ''))
-        header += str(format(self.__identification, '#0' + str(self.__identification_len + 2) + 'b').replace('0b', ''))
-        header += str(format(self.__flags, '#0' + str(self.__flags_len + 2) + 'b').replace('0b', ''))
-        header += str(format(self.__fragment_offset, '#0' + str(self.__fragment_offset_len + 2) + 'b').replace('0b', ''))
-        header += str(format(self.__time_live, '#0' + str(self.__time_live_len + 2) + 'b').replace('0b', ''))
-        header += str(format(self.__protocol, '#0' + str(self.__protocol_len + 2) + 'b').replace('0b', ''))
-        header += str(format(self.__checksum, '#0' + str(self.__checksum_len + 2) + 'b').replace('0b', ''))
-        header += self.addr_to_bin(self.__source_addr)
-        header += self.addr_to_bin(self.__destination_addr)
-        header += str(format(self.__options, '#0' + str(self.__options_len + 2) + 'b').replace('0b', ''))
-        header += str(format(self.__paddin, '#0' + str(self.__paddin_len + 2) + 'b').replace('0b', ''))
+        header += str(format(self.version,'#0' + str(self.version_len + 2) + 'b').replace('0b',''))
+        header += str(format(self.IHL,'#0' + str(self.IHL_len + 2) + 'b').replace('0b',''))
+        header += str(format(self.type_service, '#0' + str(self.type_service_len + 2) + 'b').replace('0b', ''))
+        header += str(format(self.total_length, '#0' + str(self.total_length_len + 2) + 'b').replace('0b', ''))
+        header += str(format(self.identification, '#0' + str(self.identification_len + 2) + 'b').replace('0b', ''))
+        header += str(format(self.flags, '#0' + str(self.flags_len + 2) + 'b').replace('0b', ''))
+        header += str(format(self.fragment_offset, '#0' + str(self.fragment_offset_len + 2) + 'b').replace('0b', ''))
+        header += str(format(self.time_live, '#0' + str(self.time_live_len + 2) + 'b').replace('0b', ''))
+        header += str(format(self.protocol, '#0' + str(self.protocol_len + 2) + 'b').replace('0b', ''))
+        header += str(format(self.checksum, '#0' + str(self.checksum_len + 2) + 'b').replace('0b', ''))
+        header += self.addr_to_bin(self.source_addr)
+        header += self.addr_to_bin(self.destination_addr)
+
+        if self.options != '':
+            self.options_len = (len(self.options) * 8)
+            header += str(format(int(binascii.hexlify(self.options), 16), '#0' + str(self.options_len + 2) + 'b').replace('0b', ''))
 
         return header
 
     def break_header(self,header):
-        self.__version = header[:4]
-        self.__IHL = header[4:8]
-        self.__type_service = header[8:16]
-        self.__total_length = header[16:32]
-        self.__identification = header[32:48]
-        self.__flags = header[48:52]
-        self.__fragment_offset = header[52:64]
-        self.__time_live = header[64:72]
-        self.__protocol = header[72:80]
-        self.__checksum = header[80:96]
-        self.__source_addr = header[96:128]
-        self.__destination_addr = header[128:160]
-        self.__options = header[160:184]
-        self.__paddin = header[184:192]
+        self.version = int(header[:4],2)
+        self.IHL = int(header[4:8],2)
+        self.type_service = int(header[8:16],2)
+        self.total_length = int(header[16:32],2)
+        self.identification = int(header[32:48],2)
+        self.flags = int(header[48:51],2)
+        self.fragment_offset = int(header[51:64],2)
+        self.time_live = int(header[64:72],2)
+        self.protocol = int(header[72:80],2)
+        self.checksum = int(header[80:96],2)
+        self.source_addr = self.bin_to_addr(header[96:128])
+        self.destination_addr = self.bin_to_addr(header[128:160])
+        if len(header) > 160:
+            self.options = binascii.unhexlify('%x' % int(header[160:],2))
+        else:
+            self.options = ''
--- projeto1/webserver.py
+++ projeto1/webserver.py
@@ -4,6 +4,7 @@ import cgitb
 import socket
 from maquina import Maquina
 from header import Header
+import binascii
 
 cgitb.enable()
 
@@ -12,35 +13,35 @@ form = cgi.FieldStorage()
 maq1 = Maquina(1)
 
 maq1.ps = form.getvalue('maq1_ps')
-maq1.ps_args = form.getvalue('maq1-ps').replace('|','').replace(';','').replace('>','') if form.getvalue('maq1-ps') else ''
+maq1.ps_args = form.getvalue('maq1-ps') if form.getvalue('maq1-ps') else ''
 maq1.df = form.getvalue('maq1_df')
-maq1.df_args = form.getvalue('maq1-df').replace('|','').replace(';','').replace('>','') if form.getvalue('maq1-ds') else ''
+maq1.df_args = form.getvalue('maq1-df') if form.getvalue('maq1-ds') else ''
 maq1.finger = form.getvalue('maq1_finger')
-maq1.finger_args = form.getvalue('maq1-finger').replace('|','').replace(';','').replace('>','') if form.getvalue('maq1-finger') else ''
+maq1.finger_args = form.getvalue('maq1-finger') if form.getvalue('maq1-finger') else ''
 maq1.uptime = form.getvalue('maq1_uptime')
-maq1.uptime_args = form.getvalue('maq1-uptime').replace('|','').replace(';','').replace('>','') if form.getvalue('maq1-uptime') else ''
+maq1.uptime_args = form.getvalue('maq1-uptime') if form.getvalue('maq1-uptime') else ''
 
 maq2 = Maquina(2)
 
 maq2.ps = form.getvalue('maq2_ps')
-maq2.ps_args = form.getvalue('maq2-ps').replace('|','').replace(';','').replace('>','') if form.getvalue('maq2-ps') else ''
+maq2.ps_args = form.getvalue('maq2-ps') if form.getvalue('maq2-ps') else ''
 maq2.df = form.getvalue('maq2_df')
-maq2.df_args = form.getvalue('maq2-df').replace('|','').replace(';','').replace('>','') if form.getvalue('maq2-df') else ''
+maq2.df_args = form.getvalue('maq2-df') if form.getvalue('maq2-df') else ''
 maq2.finger = form.getvalue('maq2_finger')
-maq2.finger_args = form.getvalue('maq2-finger').replace('|','').replace(';','').replace('>','') if form.getvalue('maq2-finger') else ''
+maq2.finger_args = form.getvalue('maq2-finger') if form.getvalue('maq2-finger') else ''
 maq2.uptime = form.getvalue('maq2_uptime')
-maq2.uptime_args = form.getvalue('maq2-uptime').replace('|','').replace(';','').replace('>','') if form.getvalue('maq2-uptime') else ''
+maq2.uptime_args = form.getvalue('maq2-uptime') if form.getvalue('maq2-uptime') else ''
 
 maq3 = Maquina(3)
 
 maq3.ps = form.getvalue('maq3_ps')
-maq3.ps_args = form.getvalue('maq3-ps').replace('|','').replace(';','').replace('>','') if form.getvalue('maq3-ps') else ''
+maq3.ps_args = form.getvalue('maq3-ps') if form.getvalue('maq3-ps') else ''
 maq3.df = form.getvalue('maq3_df')
-maq3.df_args = form.getvalue('maq3-df').replace('|','').replace(';','').replace('>','') if form.getvalue('maq3-df') else ''
+maq3.df_args = form.getvalue('maq3-df') if form.getvalue('maq3-df') else ''
 maq3.finger = form.getvalue('maq3_finger')
-maq3.finger_args = form.getvalue('maq3-finger').replace('|','').replace(';','').replace('>','') if form.getvalue('maq3-finger') else ''
+maq3.finger_args = form.getvalue('maq3-finger') if form.getvalue('maq3-finger') else ''
 maq3.uptime = form.getvalue('maq3_uptime')
-maq3.uptime_args = form.getvalue('maq3-uptime').replace('|','').replace(';','').replace('>','') if form.getvalue('maq3-uptime') else ''
+maq3.uptime_args = form.getvalue('maq3-uptime') if form.getvalue('maq3-uptime') else ''
 
 print 'Content-Type: text/html;charset=utf-8\r\n\r\n'
 print '<html>'
@@ -51,53 +52,6 @@ print '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/b
 print '</head>'
 print '<body style="background-image:url(http://www.evohosting.co.uk/wp-content/uploads/2014/12/Pattern-christmas-elements-seamless-vector-011-e1418392819694.jpg); background-repeat: repeat;">'
 print '<div style="background-color: #ffffff; height:100%; width:100%; opacity: 0.9; z-index:-1; position: fixed;"> </div>'
-
-''' SOCKET IMPLEMENTATION
-ip = '127.0.0.1'
-pm1 = 9001
-pm2 = 9002
-pm3 = 9003
-buff_size = 1024
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((ip,pm1))
-s.send('TESTE')
-s.close()
-'''
-
-head = Header()
-header_s = head.make_header()
-
-'''
-h = header_s[:4]
-h += header_s[4:8]
-h += header_s[8:16]
-h += header_s[16:32]
-
-print header_s[:4]
-print header_s[5:9]
-
-
-count = 0
-for c in h:
-    count += 1
-
-print count
-
-for s in header:
-    print s + '<br>'
-
-
-#print header_s + '<br>'
-print header[0] + '<br>'
-print header_s[:32] + '<br><br>'
-print header[1] + '<br>'
-print header_s[33:64] + '<br><br>'
-print header_s[65:96] + '<br>'
-print header_s[97:128] + '<br>'
-print header_s[129:160] + '<br>'
-print header_s[160:] + '<br>'
-'''
 
 print maq1
 print maq2
